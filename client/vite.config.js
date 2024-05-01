4d7ec873ccce9d4cd3fff541020d2950eb30eb36
Hsouna20 <mrabethassen012@gmail.com>
2024-05-02 01:42:01 +0200

    new file:   Dockerfile 	new file:   client/Dockerfile 	modified:   client/vite.config.js 	new file:   docker-compose.yml

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 
   server:{
+    host:true , 
+    strictPort: true , 
+    port : 86,
     proxy :{
       '/api':{
         target : 'http://localhost:3007' ,
